d63ff68c77ee0932c3510288b3cada7990362c07
Alexandr Lysenko <ovie48@mail.ru>
2023-09-17 22:06:11 +0300

    add selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,60 @@ import allure
 from allure_commons.types import AttachmentType
 from curlify import to_curl
 from requests import sessions
+import pytest
+from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
 
 resources_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resources'))
 
 token = os.getenv('TOKEN')
 chat_id = os.getenv('CHAT_ID')
+
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
+    browser.config.base_url = 'https://demoqa.com'
+
+    yield browser
+
+    browser.quit()
 
 
 def base_url(url):
